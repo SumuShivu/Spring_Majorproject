23b69fab490cc29e735b7b524daa043055347f75
SumuShivu <sumushivu5504@gmail.com>
2026-01-30 09:24:47 +0530

    feat(auth):Add PasswordEncoder configuration using BCrypt

--- src/main/java/com/example/auth_service/config/PasswordConfig.java
+++ src/main/java/com/example/auth_service/config/PasswordConfig.java
@@ -1,14 +1,12 @@
 package com.example.auth_service.config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class PasswordConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
